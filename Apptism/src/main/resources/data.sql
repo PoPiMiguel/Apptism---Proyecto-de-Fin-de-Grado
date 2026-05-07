207bc74ea51d3d957ebc7a14c11f1334d8b59c6a
Miguel Porras Pizarro <151204329+PoPiMiguel@users.noreply.github.com>
2026-05-07 14:42:51 +0000

    Revision y limpieza

--- Apptism/src/main/resources/data.sql
+++ Apptism/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-/* Este archivo se ejecuta al iniciar la app e inserta el usuario administrador en caso de no existir*/
-
 INSERT INTO usuarios (nombre, email, password, rol, puntos_acumulados)
 SELECT 'Administrador', 'admin@apptism.com', 'admin123', 'ADMIN', 0
 WHERE NOT EXISTS (
